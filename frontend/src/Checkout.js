25e530b15e212fa347b7cb1c289ea692526ea5bf
Sagar Khadka <sagarkhadka2069@gmail.com>
2020-11-14 12:51:36 +0545

    Second commit

---
+++ frontend/src/Checkout.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Checkout.css";
+function Checkout() {
+    return (
+        <div className="checkout">
+            <h1>Welcome to the checkout page.</h1>
+        </div>
+    );
+}
+export default Checkout;
